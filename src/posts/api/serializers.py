c9a249e4f6cb0f9d0e551490f0e0f7404fa507ea
CFE <hello@teamcfe.com>
2016-04-25 22:04:15 -0700

    5 - Serializing Objects in the Python Shell

--- src/posts/api/serializers.py
+++ src/posts/api/serializers.py
@@ -13,3 +13,23 @@ class PostSerializer(ModelSerializer):
             'content',
             'publish'
         ]
+
+
+""""
+
+data = {
+    "title": "Yeahh buddy",
+    "content": "New content",
+    "publish": "2016-2-12",
+    "slug": "yeah-buddy",
+    
+}
+
+new_item = PostSerializer(data=data)
+if new_item.is_valid():
+    new_item.save()
+else:
+    print(new_item.errors)
+
+
+"""
