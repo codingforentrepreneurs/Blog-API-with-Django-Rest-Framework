76eb764398628691a2be59bc87a541995913e2d7
CFE <hello@teamcfe.com>
2016-04-26 13:43:37 -0700

    11 - Custom Permissions

---
+++ src/posts/api/permissions.py
@@ -0,0 +1,16 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+
+class IsOwnerOrReadOnly(BasePermission):
+    message = 'You must be the owner of this object.'
+    my_safe_method = ['GET', 'PUT']
+    def has_permission(self, request, view):
+        if request.method in self.my_safe_method:
+            return True
+        return False
+
+    def has_object_permission(self, request, view, obj):
+       
+        if request.method in SAFE_METHODS:
+            return True
+        return obj.user == request.user
--- src/posts/api/views.py
+++ src/posts/api/views.py
@@ -7,7 +7,18 @@ from rest_framework.generics import (
     RetrieveUpdateAPIView
     )
 
+from rest_framework.permissions import (
+    AllowAny,
+    IsAuthenticated,
+    IsAdminUser,
+    IsAuthenticatedOrReadOnly,
+
+    )
+
 from posts.models import Post
+
+from .permissions import IsOwnerOrReadOnly
+
 from .serializers import (
     PostCreateUpdateSerializer, 
     PostDetailSerializer, 
@@ -18,6 +29,7 @@ from .serializers import (
 class PostCreateAPIView(CreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostCreateUpdateSerializer
+    permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
@@ -29,13 +41,16 @@ class PostDetailAPIView(RetrieveAPIView):
     lookup_field = 'slug'
     #lookup_url_kwarg = "abc"
 
+
 class PostUpdateAPIView(RetrieveUpdateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostCreateUpdateSerializer
     lookup_field = 'slug'
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
     #lookup_url_kwarg = "abc"
     def perform_update(self, serializer):
         serializer.save(user=self.request.user)
+        #email send_email
 
 
 
