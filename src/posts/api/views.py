7b3edd6f4447940dd2e70fab01af1373284c9b10
CFE <hello@teamcfe.com>
2016-04-26 17:23:43 -0700

    12 - Filtering a Queryset in a ListAPIView

--- src/posts/api/views.py
+++ src/posts/api/views.py
@@ -1,3 +1,10 @@
+from django.db.models import Q
+
+
+from rest_framework.filters import (
+        SearchFilter,
+        OrderingFilter,
+    )
 from rest_framework.generics import (
     CreateAPIView,
     DestroyAPIView,
@@ -62,10 +69,33 @@ class PostDeleteAPIView(DestroyAPIView):
 
 
 class PostListAPIView(ListAPIView):
-    queryset = Post.objects.all()
     serializer_class = PostListSerializer
+    filter_backends= [SearchFilter, OrderingFilter]
+    search_fields = ['title', 'content', 'user__first_name']
 
-    #def get_queryset()
+    def get_queryset(self, *args, **kwargs):
+        #queryset_list = super(PostListAPIView, self).get_queryset(*args, **kwargs)
+        queryset_list = Post.objects.all()
+        query = self.request.GET.get("q")
+        if query:
+            queryset_list = queryset_list.filter(
+                    Q(title__icontains=query)|
+                    Q(content__icontains=query)|
+                    Q(user__first_name__icontains=query) |
+                    Q(user__last_name__icontains=query)
+                    ).distinct()
+        return queryset_list
+
+
+
+
+
+
+
+
+
+
+
 
 
 
