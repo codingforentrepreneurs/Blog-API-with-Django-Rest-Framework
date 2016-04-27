8bf7722f0de63367647c6807f636118bcbe01473
CFE <hello@teamcfe.com>
2016-04-27 13:10:39 -0700

    19 - Comment Create Serializer Function Part 1

--- src/comments/api/serializers.py
+++ src/comments/api/serializers.py
@@ -1,12 +1,49 @@
+from django.contrib.contenttypes.models import ContentType
+
+
 from rest_framework.serializers import (
     HyperlinkedIdentityField,
     ModelSerializer,
-    SerializerMethodField
+    SerializerMethodField,
+    ValidationError
     )
 
 
 from comments.models import Comment
 
+
+def create_comment_serializer(model_type='post', slug=None, parent_id=None):
+    class CommentCreateSerializer(ModelSerializer):
+        class Meta:
+            model = Comment
+            fields = [
+                'id',
+                'parent',
+                'content',
+                'timestamp',
+            ]
+        def __init__(self, *args, **kwargs):
+            self.model_type = model_type
+            self.slug = slug
+            self.parent_obj = None
+            if self.parent_id:
+                parent_qs = Comment.objects.filter(id=parent_id)
+                if parent_qs.exists() and parent_qs.count() ==1:
+                    self.parent_obj = parent_qs.first()
+            return super(CommentCreateSerializer, self).__init__(*args, **kwargs)
+
+        def validate(self, data):
+            model_type = self.model_type
+            model_qs = ContentType.objects.filter(model=model_type)
+            if not model_qs.exists() or model_qs.count() != 1:
+                raise ValidationError("This is not a valid content type")
+            SomeModel = model_qs.first().model_class()
+            obj_qs = SomeModel.objects.filter(slug=self.slug)
+            if not obj_qs.exists() or obj_qs.count() != 1:
+                raise ValidationError("This is not a slug for this content type")
+            return data
+
+    return CommentCreateSerializer
 
 class CommentSerializer(ModelSerializer):
     reply_count = SerializerMethodField()
