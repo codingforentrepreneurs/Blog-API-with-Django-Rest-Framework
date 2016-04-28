abb5c20c33874895c52f625d04f22dedb7b0a953
CFE <hello@teamcfe.com>
2016-04-28 13:40:34 -0700

    25 - ModelSerializer Create Method

--- src/accounts/api/serializers.py
+++ src/accounts/api/serializers.py
@@ -17,9 +17,25 @@ class UserCreateSerializer(ModelSerializer):
         model = User
         fields = [
             'username',
-            'password',
             'email',
+            'password',
+            
         ]
         extra_kwargs = {"password":
                             {"write_only": True}
                             }
+    
+    def create(self, validated_data):
+        username = validated_data['username']
+        email = validated_data['email']
+        password = validated_data['password']
+        user_obj = User(
+                username = username,
+                email = email
+            )
+        user_obj.set_password(password)
+        user_obj.save()
+        return validated_data
+
+
+
